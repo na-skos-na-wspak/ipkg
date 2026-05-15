d85162e75065854e5f1fae14e8b399406576aeee
na-skos-na-wspak <outlookdawid@gmail.com>
2026-05-15 17:43:39 +0100

    Update

--- ipkg.sh
+++ ipkg.sh
@@ -1,39 +1,64 @@
 #!/usr/bin/env sh
 
-check_root() {
-	if [[ $(whoami) != 'root' ]]; then
-		printf "Please run as root\n"
-		exit 1
+cmd_install() {
+	if [[ $is_local == 'y' ]]; then
+		printf "Local installer"
+	else
+		printf "Installer"
 	fi
 }
 
-cmd_install() {
-	printf "Installer"
-}
-
 cmd_remove() {
-	printf "Remover"
+	if [[ $is_local == 'y' ]]; then
+		printf "Local remover"
+	else
+		printf "Remover"
+	fi
 }
 
 cmd_update() {
-	printf "Updater"
+	if [[ $is_local == 'y' ]]; then
+		printf "Local updater"
+	else
+		printf "updater"
+	fi
 }
 
 cmd_upgrade() {
-	printf "Upgrader"
+	if [[ $is_local == 'y' ]]; then
+		printf "Local upgrader"
+	else
+		printf "Upgrader"
+	fi
+}
+
+cmd_bootstrap() {
+	if [[ $is_local == 'y' ]]; then
+		printf "Local bootstrap"
+	else
+		printf "Bootstrap"
+	fi
 }
 
 cmd_help() {
 	y='\e[0;33m'
 	nc='\e[0m'
-	printf "$y\aIchii$nc Linux Package Manager
-A source based package manager for use on $y\aIchii$nc Linux.\n
-    install - Installs a package
-    remove  - Removes a package
-    update  - Checks installed packages for updates
-    upgrade - Updates ipkg's repositories
-    help    - Shows this help menu
+	bo='\e[1m'
+	if [[ $1 == '' ]]; then
+		exec printf "$y\aIchii$nc Linux Package Manager
+A source based package manager and bootstrapper for use on $y\aIchii$nc Linux.\n
+    $bo\ainstall$nc   -  Installs a package
+    $bo\aremove$nc    -  Removes a package
+    $bo\aupdate$nc    -  Checks installed packages for updates
+    $bo\aupgrade$nc   -  Updates ipkg's repositories
+    $bo\abootstrap$nc -  Bootstraps Ichii Linux
+    $bo\ahelp$nc      -  Shows this help menu
 "
+	fi
+
+	if [[ $1 == 'help' ]]; then
+		exec man ipkg
+	fi
 }
 
 if [[ $1 = '' ]]; then
@@ -42,9 +67,37 @@ if [[ $1 = '' ]]; then
 fi
 
 command_check() {
-	cmds=(install remove update upgrade help)
+	cmds=(install remove update upgrade bootstrap help)
 	
+	if [[ $1 == 'help' ]]; then
+		cmd_help $2
+		exit 0
+	fi
+
 	if [[ ${cmds[*]} =~ $1 ]] ; then
+		if [[ $(whoami) != 'root' ]]; then
+			while [[ $is_local != 'y' ]]; do
+
+				if [[ $is_local == '' ]]; then
+					read -rp "You are not root, would you like to go to local mode? [y/N] " is_local
+
+					if [[ $is_local == '' ]]; then
+						is_local='n'
+					fi
+
+				else read -rp "Baka! I don't understand '$is_local', give a real option! [y/N] " is_local
+
+				if [[ $is_local == '' ]]; then
+					is_local='n'
+				fi
+
+				fi
+
+				if [[ $is_local == 'n' ]]; then
+					exit 1
+				fi
+			done
+		fi
 		cmd_$1
 	else
 		cmd_help
@@ -53,4 +106,4 @@ command_check() {
 
 }
 
-command_check $1
+command_check $1 $2 $is_local
